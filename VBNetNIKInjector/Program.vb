8812806857e297877219b5163d2a144278df1535
PearlBlack42 <53957318+PearlBlack42@users.noreply.github.com>
2025-04-23 15:19:06 +0700

    Add VBNetNIKInjector/Program.vb 1745396345562

--- VBNetNIKInjector/Program.vb
+++ VBNetNIKInjector/Program.vb
@@ -1,11 +1,13 @@
 Imports System
 Imports System.Windows.Forms
 
-Module Program
-    <STAThread()>
-    Sub Main()
-        Application.EnableVisualStyles()
-        Application.SetCompatibleTextRenderingDefault(False)
-        Application.Run(New Form1())
-    End Sub
-End Module
+Namespace VBNetNIKInjector
+    Public Module Program
+        <System.STAThread()>
+        Public Sub Main()
+            Application.EnableVisualStyles()
+            Application.SetCompatibleTextRenderingDefault(False)
+            Application.Run(New Form1())
+        End Sub
+    End Module
+End Namespace
